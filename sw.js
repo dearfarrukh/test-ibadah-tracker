b160724678c6eb3f1f9af6ba2cf64f0dc8ff8714
dearfarrukh <dearfarrukh@yahoo.com>
2026-05-20 11:02:20 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -96,7 +96,8 @@ self.addEventListener("fetch", function(event){
       url.pathname.includes("/QuranPages/") ||
       url.pathname.includes("/books/") ||
       url.pathname.includes("/books/AsanNamaz/") ||
-      url.pathname.includes("/books/noorani-qaida/")
+           url.pathname.includes("/books/noorani-qaida/") ||
+      url.pathname.includes("/settings/")
     ){
       event.respondWith(
         fetch(request).catch(function(){
